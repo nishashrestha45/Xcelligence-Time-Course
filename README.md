a82fcb4fe8c537fdd6c67ee9f272c0eef1c6ddd3
Nisha Shrestha <nishashrestha45@gmail.com>
2022-09-06 22:50:16 -0500

    latest extract code

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Xcelligence-Time-Course
 This is to generate Time Course data from Xcelligence.
+
+To find the path where to put the folder:
+
+- Click data (see left panel)
+
+- Right click " Reveal in File Explorer" 
+
+- Move your data that need to be analysed to that "data" folder
+
+- Change Line number # 10 on the "extract.py" file to include the new data source of file.
+
+- Open TERMINAL on VS Code, if that is not visible vlivk on "ctrl+`" to show the hidden TERMINAL.
+
+- Run: `source venv/Scripts/activate` to activate the Python virtual environment.
+
+- Run: `python extract.py` to get output on "data/output.xlsx".
+
+
--- old_extract_code/extract.py
+++ old_extract_code/extract.py
@@ -7,10 +7,7 @@ from pandas.core.dtypes.missing import array_equivalent
 
 def main():
     all_data = pd.DataFrame()
-    for f in glob.glob("data/Xcelligence_031721_T47DM_13C3-A_ ADC_Export.xlsx"):
-        file_name = f.rsplit('/', 1)[1]
-        # print('data/output_for_' + file_name)
-        
+    for f in glob.glob("data/20220718_T47D_T3M4_8H3_Export.xlsx"):
         df = pd.read_excel(f, 'Cell Index', header=[1]) # ,index_col=0, na_values=['NA'], header=[1], usecols="B:M"
         # print(df[df.columns[0]])
         # print(df[:8])
@@ -19,21 +16,21 @@ def main():
         all_data = all_data.append(df, ignore_index=True, sort=False)
         # print(all_data)
         
-        # all_data['1,2'] = all_data[[1, 2]].mean(axis=1)
-        # all_data['3,4'] = all_data[[3, 4]].mean(axis=1)
-        # all_data['5,6'] = all_data[[5, 6]].mean(axis=1)
-        # all_data['7,8'] = all_data[[7, 8]].mean(axis=1)
-        # all_data['9,10'] = all_data[[9, 10]].mean(axis=1)
-        # all_data['11,12'] = all_data[[11, 12]].mean(axis=1)
+        all_data['1,2'] = all_data[[1, 2]].mean(axis=1)
+        all_data['3,4'] = all_data[[3, 4]].mean(axis=1)
+        all_data['5,6'] = all_data[[5, 6]].mean(axis=1)
+        all_data['7,8'] = all_data[[7, 8]].mean(axis=1)
+        all_data['9,10'] = all_data[[9, 10]].mean(axis=1)
+        all_data['11,12'] = all_data[[11, 12]].mean(axis=1)
 
         # print(all_data.at['A', '11,12'])
-        # all_data.drop(columns=[1,2,3,4,5,6,7,8,9,10,11,12], inplace=True) 
+        all_data.drop(columns=[1,2,3,4,5,6,7,8,9,10,11,12], inplace=True) 
 
         all_data.set_index(all_data.columns[0], inplace=True)
 
-        # print(all_data[:25])        
+        # print(all_data[:25])
 
-        writer = pd.ExcelWriter('output/output_for_' + file_name)
+        writer = pd.ExcelWriter('data/output.xlsx')
 
         by_conc = all_data.groupby(level=0)
         for conc, frame in by_conc:                     
