3ae646f8c94c259af1ffe7370ff8257d392b923b
Milstein Munakami <milsonmun@gmail.com>
2022-09-12 07:48:59 -0400

    tested with new time series

--- extract.py
+++ extract.py
@@ -1,22 +1,37 @@
 import pandas as pd
-import numpy as np
 import glob
-
-from pandas.core.dtypes.missing import array_equivalent
-
 
 def main():
     all_data = pd.DataFrame()
-    for f in glob.glob("data/ADC/20220718_T47D_T3M4_8H3_Export.xlsx"):
+    for f in glob.glob("data/ADC/20220307_T47DM_Levana_QC_Export.xlsx"):
         file_name = f.rsplit('/', 1)[1]
-        # print('data/output_for_' + file_name)
-        
         df = pd.read_excel(f, 'Cell Index', header=[1]) # ,index_col=0, na_values=['NA'], header=[1], usecols="B:M"
-        # print(df[df.columns[0]])
-        # print(df[:8])
-        df.dropna(inplace=True)
+        # print(df[:10])
+        # print(df[df.columns[0]][:22])
 
-        all_data = all_data.append(df, ignore_index=True, sort=False)
+        contain_values = df[df[df.columns[0]].astype(str).str.contains('Cell Index at:')].iloc[:, 0]
+        # print(contain_values[:1])   
+        initial_time = pd.Series(['0:00:00'])
+        # contain_values = initial_time.append(contain_values, ignore_index=True)
+        contain_values = pd.concat([initial_time, contain_values], ignore_index=True)
+        contain_values = contain_values.str.split('at:').str[-1]
+
+        # print(contain_values)
+
+        # new time frame with split HH:MM:SS value columns
+        total_time = contain_values.str.split(":", n = 2, expand = True)
+        # print(total_time)
+
+        # new total hours
+        contain_values = total_time[0].astype(int) + total_time[1].astype(int)/60 + total_time[2].astype(int)/3600
+
+        # print(contain_values)
+        
+        df.dropna(inplace=True)
+        # print(df[:12])
+
+        # all_data = all_data.append(df, ignore_index=True, sort=False)
+        all_data = pd.concat([df, all_data], ignore_index=True, sort=False)        
         # print(all_data)
         
         # all_data['1,2'] = all_data[[1, 2]].mean(axis=1)
@@ -30,15 +45,20 @@ def main():
         # all_data.drop(columns=[1,2,3,4,5,6,7,8,9,10,11,12], inplace=True) 
 
         all_data.set_index(all_data.columns[0], inplace=True)
+        # print(all_data[:25])
 
-        # print(all_data[:25])        
-
-        writer = pd.ExcelWriter('output/output_for_' + file_name)
+        writer = pd.ExcelWriter('data/output.xlsx')
 
         by_conc = all_data.groupby(level=0)
-        for conc, frame in by_conc:                     
+        # print(by_conc)
+   
+        for conc, frame in by_conc:          
             frame.reset_index(inplace=True, drop=True)
-            frame.index = frame.index.set_names(['Time(in hours)']) * 0.25
+            frame.insert(0, 'Time(in hours)', contain_values)
+            frame.index = frame['Time(in hours)']
+            frame.drop(columns =['Time(in hours)'], inplace = True)
+
+            # frame.index = frame.index.set_names(['Time(in hours)']) * 0.25
             
             print(f"All entries for {conc!r}")
             print("------------------------")
